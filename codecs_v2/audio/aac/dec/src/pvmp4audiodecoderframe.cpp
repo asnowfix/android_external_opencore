bce1ebb193bfcca358e68208b20ab81de97e92ec
Juan Escamilla <c_juane@quicinc.com>
2009-07-27 18:44:22 -0700

    Opencore: PacketVideo's AAC software decoder checks SBR in bitstream only.
    This change applies for AAC bitstreams handled by PacketVideo's
    AAC software decoder. With this change, PacketVideo's AAC software
    decoder verification system will ignore SBR flag on meta data and will
    only check for SBR elements on AAC audio bitstream.
    
    CRs-Fixed: 189888

--- codecs_v2/audio/aac/dec/src/pvmp4audiodecoderframe.cpp
+++ codecs_v2/audio/aac/dec/src/pvmp4audiodecoderframe.cpp
@@ -781,8 +781,7 @@ OSCL_EXPORT_REF Int PVMP4AudioDecodeFrame(
 #ifdef AAC_PLUS
     if ((pVars->bno <= 1) && (!empty_frame))
     {
-        if ((pVars->mc_info.ExtendedAudioObjectType == MP4AUDIO_AAC_LC) &&
-                (!sbrBitStream->NrElements))
+        if(!sbrBitStream->NrElements)
         {
             PVMP4AudioDecoderDisableAacPlus(pExt, pMem);
         }
