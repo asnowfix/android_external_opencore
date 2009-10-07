da595e1a744ae1e78cead8c7f113627f8a8f5a90
Adrian Salido <c_asalid@quicinc.com>
2009-10-07 13:02:58 -0700

    opencore: Set correct timestamp for auto pause while streaming
    Media timescale was not considered when setting autopause timestamp, this is
    causing abnormal timestamps to be set for autoresume when media timescale is
    not 1000ms. This happens when buffering on slow network connections.
    
    CRs-Fixed: 212037

--- nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
+++ nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
@@ -4989,7 +4989,6 @@ bool PVMFMP4FFParserNode::RetrieveTrackData(PVMP4FFNodeTrackPortInfo& aTrackPort
 
             if (!autopaused)
             {
-                uint32 requestedTimestamp = aTrackPortInfo.iTimestamp;
                 // If Parser library reported Insufficient data after seek, the requested
                 // timestamp here should be TS of sample from new position, so peek the
                 // sample.
@@ -5003,9 +5002,13 @@ bool PVMFMP4FFParserNode::RetrieveTrackData(PVMP4FFNodeTrackPortInfo& aTrackPort
                             && (numSamples > 0))
                     {
                         aTrackPortInfo.iClockConverter->set_clock(info.ts, 0);
-                        requestedTimestamp = aTrackPortInfo.iClockConverter->get_converted_ts(1000);
                     }
                 }
+                else
+                {
+                    aTrackPortInfo.iClockConverter->set_clock(aTrackPortInfo.iTimestamp, 0);
+                }
+                uint32 requestedTimestamp = aTrackPortInfo.iClockConverter->get_converted_ts(1000);
 
                 if ((NULL != iDataStreamInterface) && (0 != iDataStreamInterface->QueryBufferingCapacity()))
                 {
