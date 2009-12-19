98c0f04f6c5bf13167ee2f3fea5982cd1c29de99
Panneer S Arumugam <c_parumu@quicinc.com>
2009-12-18 18:44:28 -0800

    opencore: Assign maximum display height and width for H263 clip.
    -This change is done to handle clips in which display height
     and width present in trackheader is different from bitstream info.
    -In this case the maximum display height and width value between
     trackheader and bitstream info is assigned.
    -This change is aplicable to both 7k and 8k targets.
    
    CRs-Fixed: 220425

--- nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
+++ nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
@@ -8392,7 +8392,7 @@ PVMFStatus PVMFMP4FFParserNode::GetVideoFrameWidth(uint32 aId, int32& aWidth, in
                         }
 
                         aWidth = width;
-                        if (aDisplayWidth == 0)
+                        if (aDisplayWidth < display_width)
                         {
                             aDisplayWidth = display_width;
                         }
@@ -8402,7 +8402,6 @@ PVMFStatus PVMFMP4FFParserNode::GetVideoFrameWidth(uint32 aId, int32& aWidth, in
                 }
             }
         }
-
     }
     else if (oscl_strncmp(trackMIMEType.get_str(), PVMF_MIME_H264_VIDEO_MP4, oscl_strlen(PVMF_MIME_H264_VIDEO_MP4)) == 0)
     {
@@ -8550,7 +8549,7 @@ PVMFStatus PVMFMP4FFParserNode::GetVideoFrameHeight(uint32 aId, int32& aHeight, 
                             return PVMFFailure;
                         }
 
-                        if (aDisplayHeight == 0)
+                        if (aDisplayHeight < display_height)
                         {
                             aDisplayHeight = display_height;
                         }
